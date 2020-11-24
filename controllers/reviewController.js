c11573e92f5aceb574b92fe416ef55cf00ee0d72
Jess Schultz <schultz.jess.a@outlook.com>
2020-11-24 12:03:32 -0500

    added /:id, /buuser to review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const{Review} = require('../models');
+const{Review} = require('../models/index');
 const validateSession = require('../middleware/validateSession');
 
+router.get("/byuser", (req, res) => {
+    let ownerid = req.body.owner_ID
+    Review.findAll({where: {owner_ID: ownerid}})
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({error: err}))
+})
 
-
+router.get("/:id", (req, res) => {
+    Review.findOne({Where:{id: req.params.id}})
+    .then(review => res.status(200).json(review))
+    .catch(err => res.status(500).json(err))
+})
 
 router.post("/addreview", async (req, res) => {
     const {gameTitle, userReview, userRating, userName, owner_ID} = req.body;//totalUserRating
+
+    console.log(avgRating)
     try{
-        
+
         let newReview = Review.create({
             gameTitle,
             userReview,
