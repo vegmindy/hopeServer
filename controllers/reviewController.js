088afc8493d208839602e941ad7b021a2470f1f6
Jess Schultz <schultz.jess.a@outlook.com>
2020-11-24 10:41:33 -0500

    POST addreview created and tested

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -6,6 +6,28 @@ const validateSession = require('../middleware/validateSession');
 
 
 
+router.post("/addreview", async (req, res) => {
+    const {gameTitle, userReview, userRating, userName, owner_ID} = req.body;//totalUserRating
+    try{
+        
+        let newReview = Review.create({
+            gameTitle,
+            userReview,
+            userRating,
+            userName,
+            owner_ID
+        });
+        
+        res.status(200).json({
+            review: newReview,
+            message: "Review submitted"
+        })
+    }
+    catch{
+        res.status(500).json({message:"Server Error"})
+    }
+})
+
 
 
 
